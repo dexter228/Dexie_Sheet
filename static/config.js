5ecf2ad894ed772f82e0d4acba14e85fcace7216
dexter228 <dexter228228@gmail.com>
2017-01-25 18:55:39 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -2,21 +2,21 @@
 // ID of the Google spreadsheet
 // ***********************************************************************************
 // Make sure your spreadsheet is published (File > Publish to the web…)
-var spreadsheetId = "1P9wMb9e0YbhcOua9RQeRCJjllf_L77uV-7i4Q0Yor0o";
+var spreadsheetId = "1XzqkPvhKHOYLOda9nsuHrpVJnwYWOu91sfWq105hJVU";
 
 // ***********************************************************************************
 // Your FC and IGN
 // ***********************************************************************************
-var friendCode = "4597-0550-9417";
-var inGameName = "Richie";
+var friendCode = "4871-8273-3223";
+var inGameName = "Dexie";
 
 // ***********************************************************************************
 // URL of your Reddit/Twitter account
 // ***********************************************************************************
-var contactUrl = "http://reddit.com/u/richi3f";
+var contactUrl = "http://reddit.com/u/dexter228";
 
 // ***********************************************************************************
 // URL of your PGL trainer icon (optional)
 // ***********************************************************************************
 // Download it from the Pokémon Global Link and re-upload to an image-sharing website
-var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/d315dac0-ae8f-11e6-a3fc-06af8a77a80d.png";
+var trainerIconUrl = "https://n-3ds1-pgl-trainericon.pokemon-gl.com/161a4c70-ad50-11e6-9941-061644453cef.png";
